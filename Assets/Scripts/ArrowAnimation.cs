02fbc2d985f81eac7d83caf46806e00ecc7c7b8f
Cristian-Mihai Rosiu <rosiucristian10@gmail.com>
2020-08-04 14:26:29 +0000

    Created MainMenu

---
+++ Assets/Scripts/ArrowAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowAnimation : MonoBehaviour
+{
+
+    public Image arrowImage;
+
+    private void Start()
+    {
+        arrowImage = GetComponent<Image>();
+    }
+    public void MakeInvisible()
+    {
+        Color tempColor = arrowImage.color;
+        tempColor.a = 0f;
+        arrowImage.color = tempColor;
+        
+    }
+
+    public void MakeVisible()
+    {
+        Color tempColor = arrowImage.color;
+        tempColor.a = 100f;
+        arrowImage.color = tempColor;
+    }
+}
+
