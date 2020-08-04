5dc3ad577c60dcdb422cd1c857261bf3e28e5e73
Cristian-Mihai Rosiu <rosiucristian10@gmail.com>
2020-08-04 15:05:18 +0000

    Fixed Main Menu button.

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,10 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
     public void PlayGame()
     {
-        Debug.Log("TEST");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
