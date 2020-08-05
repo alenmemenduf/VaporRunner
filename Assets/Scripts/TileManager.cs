49c1a291121b32aea6152676cf969c566da70b7d
Alen Memenduf <alenmemenduf@gmail.com>
2020-08-05 23:59:11 +0000

    obstacle now depends on the last obstacle, not on the current corridor, code go brrr

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -1,51 +1,78 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class TileManager : MonoBehaviour
 {
     public GameObject[] tilePrefabs;
     public float zSpawn = 0;
-    public float tileLength = 12f;
-    public int numberOfTiles = 20;
-    private List<GameObject> activeTiles = new List<GameObject>();
+    public float corridorLength = 12f;
+    public int numberOfCorridors = 20;
+    public int numberOfObstacles = 10;
     public Transform playerTransform;
     public PlayerMovement player;
+    public float obstacleDespawnOffset = 3f;
+    public float zPositionOfFirstObject = 40f;
+    public float distance = 2f;
+
+    private bool firstObjectSpawned = false;
+    private List<GameObject> activeCorridors = new List<GameObject>();   //This list contains tilePrefabs[0] which should be our corridor prefab.
+    private List<GameObject> obstacles = new List<GameObject>();    //This list contains every other prefab, our obstacles.
 
     void Start()
     {
-        for(int i=0;i<numberOfTiles;i++)
-            spawnTile();
-    }
+        for (int i=0;i<numberOfCorridors;i++)
+            spawnCorridor();
+        for (int i = 0; i < numberOfObstacles; i++)
+            spawnObstacle();
+    }// We spawn a couple of corridors / obstacles at first
     void Update()
     {
-        if (playerTransform.position.z - tileLength > zSpawn - (numberOfTiles * tileLength))
+        if (playerTransform.position.z - corridorLength > zSpawn - (numberOfCorridors * corridorLength))    //When the player passes a corridor, spawn a corridor, then delete the one it already passed
         {
-            spawnTile();
-            deleteTile();
+            spawnCorridor();
+            deleteCorridor();
+        }
+        if (playerTransform.position.z > obstacles[0].transform.position.z + obstacleDespawnOffset)    // if the player passed an obstacle by some offset, destroy it and spawn another one at the end
+        {
+            spawnObstacle();
+            deleteObstacle();
         }
     }
-    public void spawnTile()
+    public void spawnCorridor()
     {
         GameObject ourGameObject = Instantiate(tilePrefabs[0], new Vector3(0f,0.0f,zSpawn), transform.rotation);
-        List<GameObject> obstacles = new List<GameObject>(3);
-            int randomIndex = Random.Range(1, tilePrefabs.Length);
-            obstacles.Add(Instantiate(tilePrefabs[randomIndex],
-                                      ourGameObject.transform.position + tilePrefabs[randomIndex].transform.position + Vector3.forward * Random.Range(1,7),
-                                      tilePrefabs[randomIndex].transform.rotation,
-                                        ourGameObject.transform));
-            
-        activeTiles.Add(ourGameObject);
-        zSpawn += tileLength;
+        activeCorridors.Add(ourGameObject);
+        zSpawn += corridorLength;
     }
-    private void deleteTile()
+    private void deleteCorridor()
     {
-        Destroy(activeTiles[0]);
-        activeTiles.RemoveAt(0);
+        Destroy(activeCorridors[0]);
+        activeCorridors.RemoveAt(0);
     }
-
-    private void speedRandomizer(){
-        Debug.Log(player.forwardMoveSpeed);
+    private void deleteObstacle()
+    {
+        Destroy(obstacles[0]);
+        obstacles.RemoveAt(0);
+    }
+    private void spawnObstacle()
+    {
+        int randomIndex = UnityEngine.Random.Range(1, tilePrefabs.Length);
+        Vector3 distanceAhead = new Vector3(0, 0, distance);
+        if (!firstObjectSpawned)
+        {
+            obstacles.Add(Instantiate(tilePrefabs[randomIndex],
+                              tilePrefabs[randomIndex].transform.position + new Vector3(0, 0, zPositionOfFirstObject),      // We add these 2 vectors because some prefabs aren't particularily well centered :). We spawn the first object at some Z distance from start.
+                              tilePrefabs[randomIndex].transform.rotation));
+            firstObjectSpawned = true;
+        }
+        else
+            obstacles.Add(Instantiate(tilePrefabs[randomIndex],
+                                  tilePrefabs[randomIndex].transform.position + new Vector3(0, 0, obstacles[obstacles.Count-1].transform.position.z + distance),    // same as above, this time, however, we use ( "the position on z of the last object" + distance) instead of the base case.
+                                  tilePrefabs[randomIndex].transform.rotation));
+            
     }
 }
