df25c5647a6788cb4276ed871f249549b842a8a7
Alen Memenduf <alenmemenduf@gmail.com>
2020-08-07 00:13:21 +0000

    globe goes round round

---
+++ Assets/RotationScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour
+{
+    public float rotationSpeed = 1f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * (rotationSpeed * Time.deltaTime));
+    }
+}
